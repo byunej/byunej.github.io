59ee564a25ed9bfdb65ecc6f57de39c42b04575c
byuneunjin <bej2816@gmail.com>
2016-12-27 16:14:02 +0900

    code_fin

--- js/src/code.js
+++ js/src/code.js
@@ -1,44 +1,78 @@
 $(function(){
   
-    $('.code_open button').on('click',function(){
+    $('.code_open_01 button').on('click',function(){
        
     $('.portfolio_01_coding').slideToggle();
         
     });
     
-    $('#portfolio_01_coding').change(function(){
+    $('#portfolio_01_coding').on('change',function(){
         
     var value = $("#portfolio_01_coding option:selected").val();
+        
         console.log(value);
         
         switch(value){
                 
             case "index_mob":
-              $('.coding_content>div').hide();  
+              $('.portfolio_01_coding .coding_content>div').hide();  
               $('.index_mob').show();
                 break;
             case "index_tab":
-              $('.coding_content>div').hide(); 
+              $('.portfolio_01_coding .coding_content>div').hide(); 
               $('.index_tab').show();
                 break;
             case "index_pc":
-              $('.coding_content>div').hide(); 
+              $('.portfolio_01_coding .coding_content>div').hide(); 
               $('.index_pc').show();
                 break;
             case "main_menu":
-              $('.coding_content>div').hide(); 
+              $('.portfolio_01_coding .coding_content>div').hide(); 
               $('.main_menu').show();
                 break;
             case "best_new":
-              $('.coding_content>div').hide(); 
+              $('.portfolio_01_coding .coding_content>div').hide(); 
               $('.best_new').show();
                 break;
             case "colorfinder":
-              $('.coding_content>div').hide(); 
+              $('.portfolio_01_coding .coding_content>div').hide(); 
               $('.colorfinder').show();
                 break;
-            
+        }
+                   
+                   });
+    
+    $('.code_open_02 button').on('click',function(){
+       
+    $('.portfolio_02_coding').slideToggle();
+        
+    });
+    
+    $('#portfolio_02_coding').on('change',function(){
+        
+    var value = $("#portfolio_02_coding option:selected").val();
+        
+        console.log(value);
+        
+        switch(value){
                 
+            case "product":
+              $('.portfolio_02_coding .coding_content>div').hide();  
+              $('.product').show();
+                break;
+            case "electronic":
+              $('.portfolio_02_coding .coding_content>div').hide(); 
+              $('.electronic').show();
+                break;
+            case "contact":
+              $('.portfolio_02_coding .coding_content>div').hide(); 
+              $('.contact').show();
+                break;
+            case "video":
+              $('.portfolio_02_coding .coding_content>div').hide(); 
+              $('.video').show();
+                break;
+            
         }
                    
                    });
